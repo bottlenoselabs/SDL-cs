ac709812ee6d62c342616ce9dd46d8fa98c4d415
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-19 23:23:02 -0400

    Fix building SDL2 on Windows

--- library.cmd
+++ library.cmd
@@ -1,18 +1,8 @@
 @echo off
 setlocal
 
-cmake -S .\src\c\sdl -B .\cmake-build-release -A x64
+cmake -S .\ext\SDL -B .\cmake-build-release -A x64
 cmake --build .\cmake-build-release --config Release
-if exist ".\lib\SDL2.exp" (
-    del /q ".\lib\SDL2.exp"
-)
-if exist ".\lib\SDL2.lib" (
-    del /q ".\lib\SDL2.lib"
-)
-if exist ".\lib\SDL2main.lib" (
-    del /q ".\lib\SDL2main.lib"
-)
-if exist ".\lib\SDL2-static.lib" (
-    del ".\lib\SDL2-static.lib"
-)
+if not exist "%~dp0\lib" mkdir %~dp0\lib
+move /y "%~dp0\cmake-build-release\Release\*.dll" "%~dp0\lib"
 rmdir /s /q .\cmake-build-release
