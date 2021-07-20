8dcf55fb516b68251afa867d44f8cb563a45c890
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-19 21:00:51 -0400

    Check if file exists for Visual Studio enterprise

--- library.cmd
+++ library.cmd
@@ -1,4 +1,14 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+@echo off
+setlocal
+
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+) else (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )
+)
+
 cmake -S .\src\c\sdl -B .\cmake-build-release -G "Visual Studio 16 2019" -DCMAKE_CONFIGURATION_TYPES=Release
 devenv .\cmake-build-release\sdl.sln /build
 if exist ".\lib\SDL2.exp" (
