42a48fb9d55d2955abd6a06dffc14f0292ba7016
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-20 16:24:16 -0400

    Update library.sh

--- library.sh
+++ library.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+script_dir="$(dirname $0)"
+echo "Started building native library... Directory: $script_dir"
 
-cmake -S ./ext/SDL -B ./cmake-build-release
-cmake --build ./cmake-build-release --config Release
-mkdir -p "./lib/"
+cmake -S $script_dir/ext/SDL -B $script_dir/cmake-build-release
+cmake --build $script_dir/cmake-build-release --config Release
+mkdir -p "$script_dir/lib/"
 
-unamestr="$(uname | tr '[:upper:]' '[:lower:]')"
-if [[ "$unamestr" == "linux" ]]; then
-    sharedobject="$(readlink -f ./cmake-build-release/libSDL2-2.0.so)"
-    mv "$sharedobject" "./lib/libSDL2.so"
-    echo "Moved $sharedobject to ./lib/libSDL2.so"
-elif [[ "$unamestr" == "darwin" ]]; then
-    mv "./cmake-build-release/libSDL2-2.0.dylib" "./lib/libSDL2.dylib"
-    echo "Moved ./cmake-build-release/libSDL2-2.0.dylib to ./lib/libSDL2.dylib"
+uname_str="$(uname | tr '[:upper:]' '[:lower:]')"
+if [[ "$uname_str" == "linux" ]]; then
+    shared_object_path="$(readlink -f $script_dir/cmake-build-release/libSDL2-2.0.so)"
+    mv "$shared_object_path" "$script_dir/lib/libSDL2.so"
+    echo "Moved $shared_object_path to $script_dir/lib/libSDL2.so"
+elif [[ "$uname_str" == "darwin" ]]; then
+    mv "$script_dir/cmake-build-release/libSDL2-2.0.dylib" "$script_dir/lib/libSDL2.dylib"
+    echo "Moved $script_dir/cmake-build-release/libSDL2-2.0.dylib to $script_dir/lib/libSDL2.dylib"
 fi
 
-rm -r ./cmake-build-release
+rm -r $script_dir/cmake-build-release
+echo "Finished building native library..."
