932b3484b31ef415f7fef8a173e0588330557b97
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-20 16:06:56 -0400

    Attempt at debugging GitHub action

--- library.sh
+++ library.sh
@@ -8,10 +8,10 @@ unamestr="$(uname | tr '[:upper:]' '[:lower:]')"
 if [[ "$unamestr" == "linux" ]]; then
     sharedobject="$(readlink -f ./cmake-build-release/libSDL2-2.0.so)"
     mv "$sharedobject" "./lib/libSDL2.so"
+    echo "Moved $sharedobject to ./lib/libSDL2.so"
 elif [[ "$unamestr" == "darwin" ]]; then
-    if [ -f "./cmake-build-release/libSDL2-2.0.dylib" ]; then
-        mv "./cmake-build-release/libSDL2-2.0.dylib" "./lib/libSDL2.dylib"
-    fi
+    mv "./cmake-build-release/libSDL2-2.0.dylib" "./lib/libSDL2.dylib"
+    echo "Moved ./cmake-build-release/libSDL2-2.0.dylib to ./lib/libSDL2.dylib"
 fi
 
 rm -r ./cmake-build-release
