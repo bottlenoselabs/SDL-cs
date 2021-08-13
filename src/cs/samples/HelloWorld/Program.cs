ede483fac056b6622967f1ef8b2d92aaab2143a2
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-08-12 21:32:22 -0400

    Testing new C2CS bindgen

--- src/cs/samples/HelloWorld/Program.cs
+++ src/cs/samples/HelloWorld/Program.cs
@@ -62,7 +62,7 @@ namespace HelloWorld
                 100,
                 800,
                 600,
-                (uint)(SDL_WindowFlags.SDL_WINDOW_SHOWN | SDL_WindowFlags.SDL_WINDOW_RESIZABLE));
+                SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 
             if (_state.Window == null)
             {
@@ -75,31 +75,31 @@ namespace HelloWorld
         private static void PrintWindowFlags(SDL_Window* window)
         {
             // See: SDL_WindowFlags @ SDL_video.h
-            var windowFlags = (SDL_WindowFlags)SDL_GetWindowFlags(window);
+            var windowFlags = SDL_GetWindowFlags(window);
 
-            Console.WriteLine(@$"Window: ""Fullscreen"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_FULLSCREEN) != 0}");
-            Console.WriteLine(@$"WindowL ""OpenGL"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_OPENGL) != 0}");
-            Console.WriteLine(@$"Window: ""Shown"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_SHOWN) != 0}");
-            Console.WriteLine(@$"Window: ""Hidden"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_HIDDEN) != 0}");
-            Console.WriteLine(@$"Window: ""Borderless"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_BORDERLESS) != 0}");
-            Console.WriteLine(@$"Window: ""Resizeable"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_RESIZABLE) != 0}");
-            Console.WriteLine(@$"Window: ""Minimized"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_MINIMIZED) != 0}");
-            Console.WriteLine(@$"Window: ""Maximized"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_MAXIMIZED) != 0}");
-            Console.WriteLine(@$"Window: ""Mouse grabbed"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_MOUSE_GRABBED) != 0}");
-            Console.WriteLine(@$"Window: ""Input focus"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS) != 0}");
-            Console.WriteLine(@$"Window: ""Mouse focus"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_MOUSE_FOCUS) != 0}");
-            Console.WriteLine(@$"Window: ""Fullscreen desktop"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_FULLSCREEN_DESKTOP) != 0}");
-            Console.WriteLine(@$"Window: ""Foreign"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_FOREIGN) != 0}");
-            Console.WriteLine(@$"Window: ""Allow high dots per inch"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_ALLOW_HIGHDPI) != 0}");
-            Console.WriteLine(@$"Window: ""Mouse capture"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_MOUSE_CAPTURE) != 0}");
-            Console.WriteLine(@$"Window: ""Always on top"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_ALWAYS_ON_TOP) != 0}");
-            Console.WriteLine(@$"Window: ""Skip taskbar"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_SKIP_TASKBAR) != 0}");
-            Console.WriteLine(@$"Window: ""Utility"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_UTILITY) != 0}");
-            Console.WriteLine(@$"Window: ""Tooltip"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_TOOLTIP) != 0}");
-            Console.WriteLine(@$"Window: ""Popup menu"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_POPUP_MENU) != 0}");
-            Console.WriteLine(@$"Window: ""Keyboard grabbed"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_KEYBOARD_GRABBED) != 0}");
-            Console.WriteLine(@$"Window: ""Vulkan"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_VULKAN) != 0}");
-            Console.WriteLine(@$"Window: ""Metal"" = {(windowFlags & SDL_WindowFlags.SDL_WINDOW_METAL) != 0}");
+            Console.WriteLine(@$"Window: ""Fullscreen"" = {(windowFlags & SDL_WINDOW_FULLSCREEN) != 0}");
+            Console.WriteLine(@$"WindowL ""OpenGL"" = {(windowFlags & SDL_WINDOW_OPENGL) != 0}");
+            Console.WriteLine(@$"Window: ""Shown"" = {(windowFlags & SDL_WINDOW_SHOWN) != 0}");
+            Console.WriteLine(@$"Window: ""Hidden"" = {(windowFlags & SDL_WINDOW_HIDDEN) != 0}");
+            Console.WriteLine(@$"Window: ""Borderless"" = {(windowFlags & SDL_WINDOW_BORDERLESS) != 0}");
+            Console.WriteLine(@$"Window: ""Resizeable"" = {(windowFlags & SDL_WINDOW_RESIZABLE) != 0}");
+            Console.WriteLine(@$"Window: ""Minimized"" = {(windowFlags & SDL_WINDOW_MINIMIZED) != 0}");
+            Console.WriteLine(@$"Window: ""Maximized"" = {(windowFlags & SDL_WINDOW_MAXIMIZED) != 0}");
+            Console.WriteLine(@$"Window: ""Mouse grabbed"" = {(windowFlags & SDL_WINDOW_MOUSE_GRABBED) != 0}");
+            Console.WriteLine(@$"Window: ""Input focus"" = {(windowFlags & SDL_WINDOW_INPUT_FOCUS) != 0}");
+            Console.WriteLine(@$"Window: ""Mouse focus"" = {(windowFlags & SDL_WINDOW_MOUSE_FOCUS) != 0}");
+            Console.WriteLine(@$"Window: ""Fullscreen desktop"" = {(windowFlags & SDL_WINDOW_FULLSCREEN_DESKTOP) != 0}");
+            Console.WriteLine(@$"Window: ""Foreign"" = {(windowFlags & SDL_WINDOW_FOREIGN) != 0}");
+            Console.WriteLine(@$"Window: ""Allow high dots per inch"" = {(windowFlags & SDL_WINDOW_ALLOW_HIGHDPI) != 0}");
+            Console.WriteLine(@$"Window: ""Mouse capture"" = {(windowFlags & SDL_WINDOW_MOUSE_CAPTURE) != 0}");
+            Console.WriteLine(@$"Window: ""Always on top"" = {(windowFlags & SDL_WINDOW_ALWAYS_ON_TOP) != 0}");
+            Console.WriteLine(@$"Window: ""Skip taskbar"" = {(windowFlags & SDL_WINDOW_SKIP_TASKBAR) != 0}");
+            Console.WriteLine(@$"Window: ""Utility"" = {(windowFlags & SDL_WINDOW_UTILITY) != 0}");
+            Console.WriteLine(@$"Window: ""Tooltip"" = {(windowFlags & SDL_WINDOW_TOOLTIP) != 0}");
+            Console.WriteLine(@$"Window: ""Popup menu"" = {(windowFlags & SDL_WINDOW_POPUP_MENU) != 0}");
+            Console.WriteLine(@$"Window: ""Keyboard grabbed"" = {(windowFlags & SDL_WINDOW_KEYBOARD_GRABBED) != 0}");
+            Console.WriteLine(@$"Window: ""Vulkan"" = {(windowFlags & SDL_WINDOW_VULKAN) != 0}");
+            Console.WriteLine(@$"Window: ""Metal"" = {(windowFlags & SDL_WINDOW_METAL) != 0}");
         }
 
         private static void CheckError(int? errorCode = -1)
