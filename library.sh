286a2c3b9904fdeb86e9ee820f2e02222644d2f5
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-09-25 13:16:36 -0400

    Update `library.sh`

--- library.sh
+++ library.sh
@@ -2,55 +2,74 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LIB_DIR="$DIR/lib"
 mkdir -p $LIB_DIR
+echo "Started '$0' $1 $2 $3 $4"
 
 if [[ ! -z "$1" ]]; then
-    TARGET_BUILD_PLATFORM="$1"
+    TARGET_BUILD_OS="$1"
 fi
 
-echo "Started '$0' $1 $2 $3"
+if [[ ! -z "$2" ]]; then
+    TARGET_BUILD_ARCH="$2"
+fi
 
-function set_target_build_platform_host() {
-    uname_str="$(uname -a)"
-    case "${uname_str}" in
-        *Microsoft*)    TARGET_BUILD_PLATFORM="microsoft";;
-        *microsoft*)    TARGET_BUILD_PLATFORM="microsoft";;
-        Linux*)         TARGET_BUILD_PLATFORM="linux";;
-        Darwin*)        TARGET_BUILD_PLATFORM="apple";;
-        CYGWIN*)        TARGET_BUILD_PLATFORM="linux";;
-        MINGW*)         TARGET_BUILD_PLATFORM="microsoft";;
-        *Msys)          TARGET_BUILD_PLATFORM="microsoft";;
-        *)              TARGET_BUILD_PLATFORM="UNKNOWN:${uname_str}"
-    esac
-}
-
-function set_target_build_platform {
-    if [[ ! -z "$TARGET_BUILD_PLATFORM" ]]; then
-        if [[ $TARGET_BUILD_PLATFORM == "default" ]]; then
-            set_target_build_platform_host
-            echo "Build platform: '$TARGET_BUILD_PLATFORM' (host default)"
-        else
-            if [[ "$TARGET_BUILD_PLATFORM" == "microsoft" || "$TARGET_BUILD_PLATFORM" == "linux" || "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
-                echo "Build platform: '$TARGET_BUILD_PLATFORM' (cross-compile override)"
-            else
-                echo "Unknown '$TARGET_BUILD_PLATFORM' passed as first argument. Use 'default' to use the host build platform or use either: 'microsoft', 'linux', 'apple'."
-                exit 1
-            fi
-        fi
+function set_target_build_os {
+    if [[ -z "$TARGET_BUILD_OS" || $TARGET_BUILD_OS == "default" ]]; then
+        uname_str="$(uname -a)"
+        case "${uname_str}" in
+            *Microsoft*)    TARGET_BUILD_OS="microsoft";;
+            *microsoft*)    TARGET_BUILD_OS="microsoft";;
+            Linux*)         TARGET_BUILD_OS="linux";;
+            Darwin*)        TARGET_BUILD_OS="apple";;
+            CYGWIN*)        TARGET_BUILD_OS="linux";;
+            MINGW*)         TARGET_BUILD_OS="microsoft";;
+            *Msys)          TARGET_BUILD_OS="microsoft";;
+            *)              TARGET_BUILD_OS="UNKNOWN:${uname_str}"
+        esac
+        echo "Target build operating system: '$TARGET_BUILD_OS' (default)"
     else
-        set_target_build_platform_host
-        echo "Build platform: '$TARGET_BUILD_PLATFORM' (host default)"
+        if [[ "$TARGET_BUILD_OS" == "microsoft" || "$TARGET_BUILD_OS" == "linux" || "$TARGET_BUILD_OS" == "apple" ]]; then
+            echo "Target build operating system: '$TARGET_BUILD_OS' (override)"
+        else
+            echo "Unknown '$TARGET_BUILD_OS' passed as first argument. Use 'default' to use the host build platform or use either: 'microsoft', 'linux', 'apple'."
+            exit 1
+        fi
     fi
 }
 
-set_target_build_platform
-if [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
+function set_target_build_arch {
+    if [[ -z "$TARGET_BUILD_ARCH" || $TARGET_BUILD_ARCH == "default" ]]; then
+        TARGET_BUILD_ARCH="$(uname -m)"
+        echo "Target build CPU architecture: '$TARGET_BUILD_ARCH' (default)"
+    else
+        if [[ "$TARGET_BUILD_ARCH" == "x86_64" || "$TARGET_BUILD_ARCH" == "arm64" ]]; then
+            echo "Target build CPU architecture: '$TARGET_BUILD_ARCH' (override)"
+        else
+            echo "Unknown '$TARGET_BUILD_ARCH' passed as second argument. Use 'default' to use the host CPU architecture or use either: 'x86_64', 'arm64'."
+            exit 1
+        fi
+    fi
+}
+
+set_target_build_os
+set_target_build_arch
+
+if [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
     CMAKE_TOOLCHAIN_ARGS="-DCMAKE_TOOLCHAIN_FILE=$DIR/mingw-w64-x86_64.cmake"
-elif [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
+elif [[ "$TARGET_BUILD_OS" == "linux" ]]; then
     CMAKE_TOOLCHAIN_ARGS=""
-elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
+elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
     CMAKE_TOOLCHAIN_ARGS=""
 else
-    echo "Unknown: $TARGET_BUILD_PLATFORM"
+    echo "Unknown target build operating system: $TARGET_BUILD_OS"
+    exit 1
+fi
+
+if [[ "$TARGET_BUILD_ARCH" == "x86_64" ]]; then
+    CMAKE_ARCH_ARGS="-A x64"
+elif [[ "$TARGET_BUILD_ARCH" == "arm64" ]]; then
+    CMAKE_ARCH_ARGS="-A arm64"
+else
+    echo "Unknown target build CPU architecture: $TARGET_BUILD_ARCH"
     exit 1
 fi
 
@@ -66,15 +85,15 @@ function build_sdl() {
     echo "Building SDL..."
     SDL_BUILD_DIR="$DIR/cmake-build-release"
     cmake $CMAKE_TOOLCHAIN_ARGS -S $DIR/ext/SDL -B $SDL_BUILD_DIR -DSDL_STATIC=OFF -DSDL_TEST=OFF -DSDL_LEAN_AND_MEAN=1
-    cmake --build $SDL_BUILD_DIR --config Release
+    cmake --build $SDL_BUILD_DIR --config Release $CMAKE_ARCH_ARGS
 
-    if [[ "$TARGET_BUILD_PLATFORM" == "linux" ]]; then
+    if [[ "$TARGET_BUILD_OS" == "linux" ]]; then
         SDL_LIBRARY_FILENAME="libSDL2.so"
         SDL_LIBRARY_FILE_PATH_BUILD="$(readlink -f $SDL_BUILD_DIR/libSDL2-2.0.so)"
-    elif [[ "$TARGET_BUILD_PLATFORM" == "apple" ]]; then
+    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
         SDL_LIBRARY_FILENAME="libSDL2.dylib"
         SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/libSDL2-2.0.dylib"
-    elif [[ "$TARGET_BUILD_PLATFORM" == "microsoft" ]]; then
+    elif [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
         SDL_LIBRARY_FILENAME="SDL2.dll"
         SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/$SDL_LIBRARY_FILENAME"
     fi
