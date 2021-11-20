4df6380164d23d643885ed2b6793c8fcffa553ca
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-11-20 10:30:08 -0500

    NuGet

--- library.sh
+++ library.sh
@@ -13,24 +13,28 @@ if [[ ! -z "$2" ]]; then
 fi
 
 function set_target_build_os {
-    if [[ -z "$TARGET_BUILD_OS" || $TARGET_BUILD_OS == "default" ]]; then
+    if [[ -z "$TARGET_BUILD_OS" || $TARGET_BUILD_OS == "host" ]]; then
         uname_str="$(uname -a)"
         case "${uname_str}" in
-            *Microsoft*)    TARGET_BUILD_OS="microsoft";;
-            *microsoft*)    TARGET_BUILD_OS="microsoft";;
+            *Microsoft*)    TARGET_BUILD_OS="windows";;
+            *microsoft*)    TARGET_BUILD_OS="windows";;
             Linux*)         TARGET_BUILD_OS="linux";;
-            Darwin*)        TARGET_BUILD_OS="apple";;
+            Darwin*)        TARGET_BUILD_OS="macos";;
             CYGWIN*)        TARGET_BUILD_OS="linux";;
-            MINGW*)         TARGET_BUILD_OS="microsoft";;
-            *Msys)          TARGET_BUILD_OS="microsoft";;
+            MINGW*)         TARGET_BUILD_OS="windows";;
+            *Msys)          TARGET_BUILD_OS="windows";;
             *)              TARGET_BUILD_OS="UNKNOWN:${uname_str}"
         esac
-        echo "Target build operating system: '$TARGET_BUILD_OS' (default)"
+        echo "Target build operating system: '$TARGET_BUILD_OS' (host)"
     else
-        if [[ "$TARGET_BUILD_OS" == "microsoft" || "$TARGET_BUILD_OS" == "linux" || "$TARGET_BUILD_OS" == "apple" ]]; then
+        if [[
+            "$TARGET_BUILD_OS" == "windows" ||
+            "$TARGET_BUILD_OS" == "macos" ||
+            "$TARGET_BUILD_OS" == "linux"
+            ]]; then
             echo "Target build operating system: '$TARGET_BUILD_OS' (override)"
         else
-            echo "Unknown '$TARGET_BUILD_OS' passed as first argument. Use 'default' to use the host build platform or use either: 'microsoft', 'linux', 'apple'."
+            echo "Unknown '$TARGET_BUILD_OS' passed as first argument. Use 'host' to use the host build platform or use either: 'windows', 'macos', 'linux'."
             exit 1
         fi
     fi
@@ -38,7 +42,11 @@ function set_target_build_os {
 
 function set_target_build_arch {
     if [[ -z "$TARGET_BUILD_ARCH" || $TARGET_BUILD_ARCH == "default" ]]; then
-        TARGET_BUILD_ARCH="$(uname -m)"
+        if [[ "$TARGET_BUILD_OS" == "macos" ]]; then
+            TARGET_BUILD_ARCH="x86_64;arm64"
+        else
+            TARGET_BUILD_ARCH="$(uname -m)"
+        fi
         echo "Target build CPU architecture: '$TARGET_BUILD_ARCH' (default)"
     else
         if [[ "$TARGET_BUILD_ARCH" == "x86_64" || "$TARGET_BUILD_ARCH" == "arm64" ]]; then
@@ -53,27 +61,24 @@ function set_target_build_arch {
 set_target_build_os
 set_target_build_arch
 
-if [[ "$TARGET_BUILD_OS" != "microsoft" && "$TARGET_BUILD_OS" != "apple" && "$TARGET_BUILD_OS" != "linux" ]]; then
+if [[
+    "$TARGET_BUILD_OS" != "windows" &&
+    "$TARGET_BUILD_OS" != "macos" &&
+    "$TARGET_BUILD_OS" != "linux"
+    ]]; then
     echo "Unknown target build operating system: $TARGET_BUILD_OS"
     exit 1
 fi
 
-if [[ "$TARGET_BUILD_ARCH" == "x86_64" ]]; then
-    if [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
+if [[ "$TARGET_BUILD_OS" == "macos" ]]; then
+    CMAKE_ARCH_ARGS="-DCMAKE_OSX_ARCHITECTURES=$TARGET_BUILD_ARCH"
+elif [[ "$TARGET_BUILD_OS" == "windows" ]]; then
+    if [[ "$TARGET_BUILD_ARCH" == "x86_64" ]]; then
         CMAKE_TOOLCHAIN_ARGS="-DCMAKE_TOOLCHAIN_FILE=$DIR/mingw-w64-x86_64.cmake"
-    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
-        CMAKE_ARCH_ARGS="-DCMAKE_OSX_ARCHITECTURES=x86_64"
-    fi
-elif [[ "$TARGET_BUILD_ARCH" == "arm64" ]]; then
-    if [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
-        echo "ARM64 not yet supported for Windows."
+    else
+        echo "Unknown target build CPU architecture for Windows: $TARGET_BUILD_ARCH"
         exit 1
-    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
-        CMAKE_ARCH_ARGS="-DCMAKE_OSX_ARCHITECTURES=arm64"
     fi
-else
-    echo "Unknown target build CPU architecture: $TARGET_BUILD_ARCH"
-    exit 1
 fi
 
 function exit_if_last_command_failed() {
@@ -93,10 +98,10 @@ function build_sdl() {
     if [[ "$TARGET_BUILD_OS" == "linux" ]]; then
         SDL_LIBRARY_FILENAME="libSDL2.so"
         SDL_LIBRARY_FILE_PATH_BUILD="$(readlink -f $SDL_BUILD_DIR/libSDL2-2.0.so)"
-    elif [[ "$TARGET_BUILD_OS" == "apple" ]]; then
+    elif [[ "$TARGET_BUILD_OS" == "macos" ]]; then
         SDL_LIBRARY_FILENAME="libSDL2.dylib"
         SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/libSDL2-2.0.dylib"
-    elif [[ "$TARGET_BUILD_OS" == "microsoft" ]]; then
+    elif [[ "$TARGET_BUILD_OS" == "windows" ]]; then
         SDL_LIBRARY_FILENAME="SDL2.dll"
         SDL_LIBRARY_FILE_PATH_BUILD="$SDL_BUILD_DIR/$SDL_LIBRARY_FILENAME"
     fi
@@ -113,7 +118,7 @@ function build_sdl() {
 
     rm -r $SDL_BUILD_DIR
     exit_if_last_command_failed
-    echo "Building sokol finished!"
+    echo "Building SDL2 finished!"
 }
 
 build_sdl
