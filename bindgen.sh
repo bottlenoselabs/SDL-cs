24e0e8fd53dfb32fbdc5853f4d0da753ee7dc190
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-12-11 20:13:29 -0500

    Update `bindgen.sh` + `SDL-cs.csproj`

--- bindgen.sh
+++ bindgen.sh
@@ -10,10 +10,12 @@ function exit_if_last_command_failed() {
 function bindgen {
     c2cs ast -i ./ext/SDL/include/SDL.h -o ./ast/SDL.json -s ./ext/SDL/include -b 64 -g \
 "begin_code.h" \
+"end_code.h" \
 "SDL_endian.h" \
 "SDL_config_android.h" \
-"SDL_config_macosx.h" \
+"SDL_config_emscripten.h" \
 "SDL_config_iphoneos.h" \
+"SDL_config_macosx.h" \
 "SDL_config_minimal.h" \
 "SDL_config_os2.h" \
 "SDL_config_pandora.h" \
@@ -21,6 +23,7 @@ function bindgen {
 "SDL_config_windows.h" \
 "SDL_config_winrt.h" \
 "SDL_config_wiz.h" \
+"SDL_config.h" \
 -d \
 SDL_DISABLE_IMMINTRIN_H \
 SDL_DISABLE_MMINTRIN_H \
@@ -30,6 +33,7 @@ SDL_DISABLE_PMMINTRIN_H \
     exit_if_last_command_failed
     c2cs cs -i ./ast/SDL.json -o ./src/cs/production/SDL-cs/SDL.cs -l "SDL2" -c "SDL" -g ./ignored.txt \
 -a \
+"_SDL_iconv_t* -> nint" \
 "SDL_bool -> CBool" \
 "Uint8 -> byte" \
 "Uint16 -> ushort" \
