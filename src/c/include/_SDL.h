b8e2aa509290bd2278a6b04c7a587c28f591555b
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-06-05 15:16:43 -0400

    Test bindgen

--- src/c/include/_SDL.h
+++ src/c/include/_SDL.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#define XSTR(x) STR(x)
+#define STR(x) #x
+#define ABC 1
+
 #if defined(__APPLE__)
     #if __has_include("AvailabilityMacros.h")
         #include <AvailabilityMacros.h>
     #endif
+
+    #if MAC_OS_X_VERSION_MIN_REQUIRED < 1060
+        # error "Test1"
+    #endif
 #else
     #define MAC_OS_X_VERSION_MIN_REQUIRED 1070
+    #if MAC_OS_X_VERSION_MIN_REQUIRED < 1060
+        # error "Test2"
+    #endif
 #endif
 
 #define SDL_DISABLE_IMMINTRIN_H
