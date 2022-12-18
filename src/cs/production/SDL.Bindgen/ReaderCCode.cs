29eaae489ce256efe72de45dd0de54535fd23a42
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2022-12-17 19:23:02 -0500

    Update `ReaderCCode.cs`

--- src/cs/production/SDL.Bindgen/ReaderCCode.cs
+++ src/cs/production/SDL.Bindgen/ReaderCCode.cs
@@ -34,11 +34,11 @@ public class ReaderCCode : IReaderCCode
     {
 	    options.HeaderFilesBlocked = new[]
 	    {
-		    "SDL_thread.h", "SDL_stdinc.h"
+		    "SDL_thread.h", "SDL_stdinc.h", "SDL_audio.h"
 	    }.ToImmutableArray();
 	    options.OpaqueTypeNames = new[]
 	    {
-		    "SDL_AudioCVT", "SDL_RWops"
+		    "SDL_RWops"
 	    }.ToImmutableArray();
 	    options.IsEnabledEnumsDangling = true;
 	    options.PassThroughTypeNames = new[]
