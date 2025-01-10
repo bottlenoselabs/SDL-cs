9e096c88d748f6667e2453a108fa751238089f30
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2025-01-09 22:20:10 -0500

    Add 02 example

---
+++ src/cs/examples/_02-GettingAnImageOnScreen/Program.cs
@@ -0,0 +1,91 @@
+// Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
+
+namespace SDL.Examples;
+
+public static unsafe class Program
+{
+    public static readonly ProgramState State = new();
+
+    private static int Main()
+    {
+        Initialize();
+
+        if (LoadMedia())
+        {
+            _ = SDL_BlitSurface(State.UserSurface, default, State.ScreenSurface, default);
+            _ = SDL_UpdateWindowSurface(State.Window); // flip back and front buffer
+        }
+
+        Loop();
+        Close();
+
+        return 0;
+    }
+
+    private static void Initialize()
+    {
+        if (!SDL_Init(SDL_INIT_VIDEO))
+        {
+            Console.Error.WriteLine("Failed to initialize SDL. SDL error: " + SDL_GetError());
+            Environment.Exit(1);
+        }
+
+        State.Window = SDL_CreateWindow(
+            (CString)"SDL Sample: Getting an image on screen"u8,
+            640,
+            480,
+            SDL_WINDOW_RESIZABLE);
+        if (State.Window == null)
+        {
+            Console.Error.WriteLine("Failed to create window. SDL error: " + SDL_GetError());
+            Environment.Exit(1);
+        }
+
+        State.ScreenSurface = SDL_GetWindowSurface(State.Window);
+    }
+
+    private static void Close()
+    {
+        SDL_DestroySurface(State.UserSurface);
+        State.UserSurface = null;
+
+        SDL_DestroyWindow(State.Window);
+        State.Window = null;
+
+        SDL_Quit();
+    }
+
+    private static void Loop()
+    {
+        while (true)
+        {
+            SDL_Event e;
+            if (!SDL_PollEvent(&e))
+            {
+                continue;
+            }
+
+            if (e.type == (ulong)SDL_EventType.SDL_EVENT_QUIT)
+            {
+                break;
+            }
+        }
+    }
+
+    private static bool LoadMedia()
+    {
+        var filePath = AppContext.BaseDirectory + "/hello_world.bmp";
+        using var filePathC = (CString)filePath;
+        State.UserSurface = SDL_LoadBMP(filePathC);
+
+        if (State.UserSurface == null)
+        {
+            Console.Error.WriteLine("Failed to load image '{0}'. SDL error: {1}", filePath, SDL_GetError());
+            Console.WriteLine("Failed to load media.");
+            return false;
+        }
+
+        return true;
+    }
+}
