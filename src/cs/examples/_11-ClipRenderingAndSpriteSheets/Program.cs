4f5189705567c07d23118241a196a879042418c4
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2025-01-23 22:45:42 -0500

    Add LazyFoo example 11 ClipRenderingAndSpriteSheets (#534)

---
+++ src/cs/examples/_11-ClipRenderingAndSpriteSheets/Program.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
+
+namespace SDL.Examples;
+
+public static class Program
+{
+    // https://lazyfoo.net/tutorials/SDL/10_color_keying/index.php
+
+    private static int Main()
+    {
+        Interop.SDL.Initialize();
+        Interop.SDL_image.Initialize();
+
+        using var app = new App();
+        app.Run();
+
+        return 0;
+    }
+}
